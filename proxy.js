3bc97cd1dbf81e91aaba47faef7792b5342fabfd
Oscarhasanputra <oscarhasanputra2@gmail.com>
2022-06-04 19:53:45 +0700

    add proxy server

---
+++ proxy.js
@@ -0,0 +1,117 @@
+const http = require("http");
+const request = require("request");
+const url = require("url");
+const Axios = require("axios");
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var path = require("path");
+var app = express();
+var logger = require("morgan");
+const cors = require("cors");
+const { default: axios } = require("axios");
+var rpc = require('node-json-rpc');
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const parseIncomingRequest = (clientRequest, clientResponse) => {
+  const requestToFulfil = url.parse(
+    "https://data-seed-prebsc-1-s1.binance.org:8545"
+  );
+  
+  // Frame the request to be forwarded via Backend to External Source
+  // const options = {
+  //   method: "POST",
+  //   headers: clientRequest.headers,
+  //   host: requestToFulfil.hostname,
+  //   port: requestToFulfil.port || 80,
+  //   path: requestToFulfil.path,
+  // };
+    const options = {
+      port:8545,
+      host:"data-seed-prebsc-1-s1.binance.org",
+      path:"/",
+      strict:true
+    }
+
+  // const options = {
+  //     method: "POST",
+  //     headers: clientRequest.headers,
+  //     host: "https://data-seed-prebsc-1-s1.binance.org:8545",
+  //     port: 8545,
+  //     searchParams:
+  //     path: "/",
+  //   };
+  console.log("parse incoming request");
+
+  executeRequest(options, clientRequest, clientResponse);
+};
+
+const executeRequest = (options, clientRequest, clientResponse) => {
+  // http.request({})
+  
+  axios.post("https://data-seed-prebsc-1-s1.binance.org:8545/",JSON.stringify(clientRequest.body),{
+    headers:{
+      "content-type": "application/json"
+    }
+  }).then(res=>{
+    clientResponse.send(res.data)
+    console.log(res)
+  }).catch(err=>{
+    console.log("error")
+    console.log(err)
+  })
+  // const externalRequest = http.request(options, (externalResponse) => {
+  //   // Write Headers to clientResponse
+  //   clientResponse.writeHead(
+  //     externalResponse.statusCode,
+  //     externalResponse.headers
+  //   );
+  //   console.log("response");
+  //   console.log(externalResponse);
+  //   // Forward the data being received from external source back to client
+  //   externalResponse.on("data", (chunk) => {
+  //     console.log("data send to client ", chunk.toString());
+  //     clientResponse.write(chunk);
+  //   });
+
+  //   // End the client response when the request from external source has completed
+  //   externalResponse.on("end", () => {
+  //     clientResponse.end();
+  //   });
+  // });
+
+  // // Map data coming from client request to the external request being made
+  // clientRequest.on("data", (chunk) => {
+  //   console.log("data send from client ", chunk.toString());
+  //   externalRequest.write(chunk);
+  // });
+
+  // // Map the end of client request to the external request being made
+  // clientRequest.on("end", () => {
+  //   externalRequest.end();
+  // });
+  // console.log("request send ");
+  // console.log(JSON.stringify(clientRequest.body));
+  // externalRequest.write(JSON.stringify(clientRequest.body));
+  // externalRequest.end();
+};
+
+// Create a HTTP server
+// app.use("/",(req,res)=>{
+//     console.log(req.method)
+//     console.log(req.body)
+// })
+
+app.use(parseIncomingRequest);
+// app.
+app.set("port", "8080");
+const server = http.createServer(app);
+server.listen(8080, () => {
+  console.log("Proxy Server listening on Port 8080");
+});
--- src/utils/SmartContractCaller.js
+++ src/utils/SmartContractCaller.js
@@ -4,85 +4,92 @@ import { ethers, Contract } from "ethers";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve,ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 export const ConnectBlockchain = (bool) =>
   new Promise(async (res, rej) => {
+    let provider = await detectEthereumProvider();
+
     if (bool) {
-      let provider = await detectEthereumProvider();
       if (provider) {
-
-        sleep(10000).then(()=>{
+        sleep(10000).then(() => {
           const network = provider.network;
-          if(!network){
-            alert("Please Using your VPN First for better experience")
+          if (!network) {
+            alert("Please Using your VPN First for better experience");
           }
-        })
-
-        try {
-        // change network to bsc testnet
-        await provider.request({
-          method: "wallet_addEthereumChain",
-          params: [
-            {
-              chainId: "0x61",
-              chainName: "BSC-Testnet",
-              rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545/"],
-              nativeCurrency: {
-                name: "BNB",
-                symbol: "BNB",
-                decimals: 18,
-              },
-            },
-          ],
         });
 
-        
+        try {
+          // change network to bsc testnet
+          await provider.request({
+            method: "wallet_addEthereumChain",
+            params: [
+              {
+                chainId: "0x61",
+                chainName: "BSC-Testnet",
+                rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545/"],
+                nativeCurrency: {
+                  name: "BNB",
+                  symbol: "BNB",
+                  decimals: 18,
+                },
+              },
+            ],
+          });
+
           const accounts = await provider.request({
             method: "eth_requestAccounts",
           });
           const networkID = await provider.request({ method: "net_version" });
 
           provider = new ethers.providers.Web3Provider(provider);
-          
+
           const signer = provider.getSigner();
           const address = await signer.getAddress();
-          
+
           const myContract = new Contract(
             MyContract.networks[networkID].address,
             MyContract.abi,
             signer
           );
-          const profil=await myContract.wallets(address);
-          res({ myContract, wallet: address, provider,profil});
+          const profil = await myContract.wallets(address);
+          res({ myContract, wallet: address, provider, profil });
           return;
         } catch (error) {
-          console.log(error)
-          rej(error)
+          console.log(error);
+          rej(error);
           alert("Login your Metamask, and Try to Refresh Page");
         }
       }
     } else {
       const networkID = 97;
+      const connection = {
+        url: "http://localhost:8080/",
+        headers: {
+          // "externalURL": "https://data-seed-prebsc-1-s1.binance.org:8545/",
+          // "X-Forwarded-For": "203.0.113.195 , localhost:8080"
+        },
+      };
+      // console.log("connection forwarded")
+      // console.log(connection);
       try {
         const provider = new ethers.providers.JsonRpcProvider(
-          "https://data-seed-prebsc-1-s1.binance.org:8545/"
+          connection
         );
         //  const net= provider.connection
         // const net=await provider.detectNetwork()
-        
+
         // console.log(net)
-        
-        sleep(10000).then(()=>{
+
+        sleep(10000).then(() => {
           const network = provider.network;
-          if(!network){
-            alert("Please Using your VPN First for better experience")
+          if (!network) {
+            alert("Please Using your VPN First for better experience");
           }
-        })
-       
-        await provider.getNetwork()
-        
-        
+        });
+
+        await provider.getNetwork();
+
         const address = null;
         const myContract = new Contract(
           MyContract.networks[networkID].address,
@@ -92,11 +99,11 @@ export const ConnectBlockchain = (bool) =>
         // console.log(myContract)
         res({ myContract, wallet: address });
       } catch (error) {
-        console.log("error smartcontract")
-        console.log(error)
-        rej(error)
+        console.log("error smartcontract");
+        console.log(error);
+        rej(error);
       }
-      
+
       // return;
     }
   });
